e2fc645aece5bf4fabbe93827d8550d81e06e6a6
Marcos Tavares <mjtdes.md@gmail.com>
2025-03-15 19:22:30 -0300

    Initial project commit

---
+++ src/test/java/dev/marcostavares/api_rest_challenge/ApiRestChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.marcostavares.api_rest_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
